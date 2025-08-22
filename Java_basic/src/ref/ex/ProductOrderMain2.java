2c4374eb7c454b29da08b67afb0a969ba5799203
Tae Kyoun Kim <xorbskim6105@gmail.com>
2025-08-22 19:10:45 +0900

    feat/풀이 해설

--- Java_basic/src/ref/ex/ProductOrderMain2.java
+++ Java_basic/src/ref/ex/ProductOrderMain2.java
@@ -4,13 +4,9 @@ public class ProductOrderMain2 {
     public static void main(String[] args) {
         ProductOrder[] productOrders = new ProductOrder[3];
 
-        ProductOrder productOrder1 = createOrder("두부",2000,2);
-        ProductOrder productOrder2 = createOrder("김치", 5000, 1);
-        ProductOrder productOrder3 = createOrder("콜라", 1500, 2);
-
-        productOrders[0] = productOrder1;
-        productOrders[1] = productOrder2;
-        productOrders[2] = productOrder3;
+        productOrders[0] = createOrder("두부",2000,2);
+        productOrders[1] = createOrder("김치", 5000, 1);
+        productOrders[2] = createOrder("콜라", 1500, 2);
 
         printOrder(productOrders);
         System.out.println("총 결제 금액: "+ getTotalAmount(productOrders));
